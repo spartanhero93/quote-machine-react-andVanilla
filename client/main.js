44634b24d0b8186adb784421f62cf914ed8383ee
luwis lopez <spartanhero93@gmail.com>
2018-05-23 20:54:11 -0400

    beta version

--- client/main.js
+++ client/main.js
@@ -1,22 +1,24 @@
-let [quote, author, quoteBtn] = [
-  document.querySelector("p"[0]),
-  document.querySelector("em"),
-  document.querySelector(".btn")
-];
+let [quotes, author, quoteBtn, JSquoteBtn] = [
+  document.querySelectorAll('p'),
+  document.querySelector('em'),
+  document.querySelector('.btn')
+]
 
-let quoteNum = 0;
+console.log(quotes.length)
+
+let quoteNum = 0
 
 const getNewQuote = () => {
   axios
-  .get("/getQuote")
-  .then(res => {
-    info = res.data
-    console.log(info)
-    author.textContent = info[quoteNum].name
-    quote.textContent = info[quoteNum].quote
-    quoteNum > info.length ? (quoteNum = 0) : quoteNum++;
-  })
-  .catch(err => console.error(err));
-};
+    .get('/getQuote')
+    .then(res => {
+      info = res.data
+      console.log(info)
+      author.textContent = info[quoteNum].name
+      quotes[0].textContent = info[quoteNum].quote
+      quoteNum > info.length ? (quoteNum = 0) : quoteNum++
+    })
+    .catch(err => console.error(err))
+}
 
-quoteBtn.addEventListener("click", getNewQuote);
+quoteBtn.addEventListener('click', getNewQuote)
